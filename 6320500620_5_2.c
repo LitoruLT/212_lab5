89b4edd6f15f1c58c9407c62312106b3df31557a
Litoru <puwarin2001@gmail.com>
2021-03-11 15:15:42 +0700

    5_2 Done

--- 6320500620_5_2.c
+++ 6320500620_5_2.c
@@ -5,32 +5,59 @@ void main()
     int hrow,col,hcol,n,i,j,k,spos;
     scanf("%d",&n);
 
-    if(2%1!=0)
+
+    if(n%2==0)
+    {
+        hrow=n/2;
+        col=n-1;
+    }
+    else
     {
         hrow=(n/2)+1;
         col=n;
     }
-    else
-    {
-        hrow=n/2;
-        col=n-1;
-        hcol=(col/2)+1;
-    }
+    hcol=col/2;
 
     for(i=0; i<hrow ;i++)
     {
         for(k=0;k<col;k++)
         {
-            if(k!=hcol)
+            if( (k == hcol+i) || (k == hcol-i) )
             {
-                printf("_");
+                printf("*");
             }
             else
             {
-               printf("*");
+                printf("_");
             }
-            printf("\n")
         }
+        printf("\n");
+    }
+
+    if(n%2==0)
+    {
+        i=i;
+    }
+    else
+    {
+        i=i-1;
+    }
+
+
+    for(i=i-1 ; i>=0 ;i=i-1)
+    {
+        for(k=0;k<col;k++)
+        {
+            if( (k == hcol+i) || (k == hcol-i) )
+            {
+                printf("*");
+            }
+            else
+            {
+                printf("_");
+            }
+        }
+        printf("\n");
     }
 
 }
