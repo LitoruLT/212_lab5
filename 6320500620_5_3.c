8f5976fb4a8b337e11c6545e31d816a5bb0f6ff3
Litoru <puwarin2001@gmail.com>
2021-03-11 15:34:22 +0700

    กำลังแก้ไข 5_3 ให้ถูก

--- 6320500620_5_3.c
+++ 6320500620_5_3.c
@@ -1,8 +1,44 @@
 
 #include<stdio.h>
 
+unsigned long long checkugly(unsigned long long num)
+{
+    while(num>1)
+    {
+        if(num%2==0)
+        {
+            num=num/2;
+        }
+        else if(num%3==0)
+        {
+            num=num/3;
+        }
+        else if(num%5==0)
+        {
+            num=num/5;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
 void main()
 {
+    unsigned long long n,i=2,j,k,count=2;
+    scanf("%llu",&n);
+
+    while(count<n)
+    {
+        printf("count=%llu\n",count);
+        printf("i=%llu\n",i);
+        count=count+checkugly(i);
+        i++;
+
+    }
+    printf("%llu",i);
 
 
 }
