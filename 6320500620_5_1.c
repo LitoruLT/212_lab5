0abaf48e70299a4b885602f3ad2c1d7417029315
Litoru <puwarin2001@gmail.com>
2021-03-11 17:15:22 +0700

    หาวิธีเก็บเลข

--- 6320500620_5_1.c
+++ 6320500620_5_1.c
@@ -1,1 +1,39 @@
+#include<stdio.h>
 
+void main()
+{
+    int n,i,ans;
+    scanf("%d",&n);
+    int high=n*n*n*n*n;
+    unsigned char term[high];
+
+    int stack[n];
+
+    fflush(stdin);
+    scanf("%[^\n]",&term);
+    int k=0;
+    while(term[i]!='\0')
+    {
+        if(term[i]=='-')
+        {
+
+        }
+        else if(term[i]=='+')
+        {
+
+        }
+        else if(term[i]=='*')
+        {
+
+        }
+        else
+        {
+            k++;
+        }
+        i++;
+
+    }
+
+
+    printf("%s",term);
+}
