5aa84edf39876878443b83bc65f638b25e6e7b80
Litoru <puwarin2001@gmail.com>
2021-03-11 16:47:18 +0700

    5-5 ยังมี bug เล็กน้อย

--- 6320500620_5_5.c
+++ 6320500620_5_5.c
@@ -17,23 +17,32 @@ void main()
         }
     }
     i=0,k=0;
+    int pcol=0,prow=0;
 
     int now=0,max=0,temp=0;
-    while(i<N && k<N)
+    while( i<N || k<N )
     {
-        for(i=0+temp ; i+temp<B+temp ; i++)
+        for(i=0+prow ; i<B+prow ; i++)
         {
-            for(k=0+temp ; k+temp<B+temp ; k++)
+            for(k=0+pcol ; k<B+pcol ; k++)
             {
+                printf(" A+ %d\n",A[i][k]);
                 now=now+A[i][k];
             }
 
         }
+        printf("now = %d\n",now);
 
         if(temp==0 || max<now)
             max=now;
         now=0;
-        temp++;
+        temp++;pcol++;
+        if(pcol>N)
+        {
+            pcol=0;
+            prow++;
+        }
+
     }
 
     printf("max= %d\n",max);
